603b4d50899c67633fcf2d90c7c79a60d4b98cf4
Federico <47187453+vulcanoFe@users.noreply.github.com>
2022-12-20 11:15:28 +0100

    bar chart
    nivo charts

--- react-admin/src/scenes/bar/index.jsx
+++ react-admin/src/scenes/bar/index.jsx
@@ -1,11 +1,15 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
+import BarChart from "../../components/BarChart";
 
 const Bar = () => {
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="BAR CHART" subtitle="Bar chart"/>
+        <Header title="BAR CHART" subtitle="Simple Bar chart"/>
+      </Box>
+      <Box height="75vh">
+        <BarChart />
       </Box>
     </Box>
   );
