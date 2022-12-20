dd8cdf5ac4465f7535c184203f0e83005b764759
Federico <47187453+vulcanoFe@users.noreply.github.com>
2022-12-20 10:33:54 +0100

    faq page
    material ui accordion

--- react-admin/src/scenes/faq/index.jsx
+++ react-admin/src/scenes/faq/index.jsx
@@ -1,12 +1,87 @@
-import { Box } from "@mui/material";
+import { Box, useTheme, Typography } from "@mui/material";
 import Header from "../../components/Header";
-
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
+import { tokens } from "../../theme";
+ 
 const FAQ = () => {
+
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="FAQ" subtitle="Frequently Asked Questions"/>
+        <Header title="FAQ" subtitle="Frequently Asked Questions Page"/>
       </Box>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, sed qui! Nisi voluptatem at voluptas eum voluptatibus, temporibus quidem consectetur nulla? Explicabo est dolor cum nam, doloribus fuga ducimus eius.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            How to .... ?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, sed qui! Nisi voluptatem at voluptas eum voluptatibus, temporibus quidem consectetur nulla? Explicabo est dolor cum nam, doloribus fuga ducimus eius.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Which type of .... ?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, sed qui! Nisi voluptatem at voluptas eum voluptatibus, temporibus quidem consectetur nulla? Explicabo est dolor cum nam, doloribus fuga ducimus eius.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            How many time I can ... ?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, sed qui! Nisi voluptatem at voluptas eum voluptatibus, temporibus quidem consectetur nulla? Explicabo est dolor cum nam, doloribus fuga ducimus eius.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Final Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, sed qui! Nisi voluptatem at voluptas eum voluptatibus, temporibus quidem consectetur nulla? Explicabo est dolor cum nam, doloribus fuga ducimus eius.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
     </Box>
   );
 };
